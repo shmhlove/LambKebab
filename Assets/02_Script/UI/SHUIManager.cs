09a868ee8426049df5d81b75e0687215f8293a62
shmhlove <shmhlove@naver.com>
2016-11-30 13:58:45 +0900

    [추가] 양꼬치

---
+++ Assets/02_Script/UI/SHUIManager.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using DicPanels = System.Collections.Generic.Dictionary<string, SHUIBasePanel>;
+
+public partial class SHUIManager : SHSingleton<SHUIManager>
+{
+    #region Members
+    [ReadOnlyField][SerializeField] private DicPanels m_dicPanels = new DicPanels();
+    #endregion
+
+
+    #region Virtual Functions
+    public override void OnInitialize()
+    {
+        SetDontDestroy();
+        Single.Scene.AddEventToChangeScene(OnEventToChangeScene);
+        SHGameObject.SetParent(Single.Resource.GetGameObject("UI Root - Global"), gameObject);
+    }
+    public override void OnFinalize()
+    {
+        
+    }
+    #endregion
+
+
+    #region System Functions
+    #endregion
+
+
+    #region Interface Functions
+    public void AddPanel(SHUIBasePanel pPanel, bool bIsActive)
+    {
+        if (null == pPanel)
+        {
+            Debug.LogError("AddPanel() - Panel Is null!!");
+            return;
+        }
+
+        if (true == m_dicPanels.ContainsKey(pPanel.name))
+            m_dicPanels[pPanel.name] = pPanel;
+        else
+            m_dicPanels.Add(pPanel.name, pPanel);
+
+        pPanel.Initialize(bIsActive);
+    }
+    public bool Show(string strName, params object[] pArgs)
+    {
+        var pPanel = GetPanel(strName);
+        if (null == pPanel)
+        {
+            Debug.LogErrorFormat("Show() - No Exist Panel(Name : {0})", strName);
+            return false;
+        }
+        
+        pPanel.Show(pArgs);
+        return true;
+    }
+    public bool Close(string strName)
+    {
+        var pPanel = GetPanel(strName);
+        if (null == pPanel)
+        {
+            Debug.LogErrorFormat("Close() - No Exist Panel(Name : {0})", strName);
+            return false;
+        }
+
+        pPanel.Close();
+        return true;
+    }
+    public bool IsExistPanel(string strName)
+    {
+        return (null != GetPanel(strName));
+    }
+    public Transform GetRootToGlobal()
+    {
+        return SHUIRoot_Global.GetRoot();
+    }
+    public Transform GetRootToScene()
+    {
+        return SHUIRoot_Scene.GetRoot();
+    }
+    #endregion
+
+
+    #region Utility Functions
+    SHUIBasePanel GetPanel(string strName)
+    {
+        if (false == m_dicPanels.ContainsKey(strName))
+        {
+            AddPanel(Single.Resource.GetObjectComponent<SHUIBasePanel>(strName), false);
+        }
+
+        if (false == m_dicPanels.ContainsKey(strName))
+        {
+            return null;
+        }
+        
+        return m_dicPanels[strName];
+    }
+    void DestoryPanel(Dictionary<string, SHUIBasePanel> dicPanels)
+    {
+        if (null == dicPanels)
+            return;
+
+        SHUtils.ForToDic(new DicPanels(dicPanels), (pKey, pValue) =>
+        {
+            DestroyPanel(pValue);
+            m_dicPanels.Remove(pKey);
+        });
+    }
+    void DestroyPanel(SHUIBasePanel pPanel)
+    {
+        if (null == pPanel)
+            return;
+
+        SHGameObject.DestoryObject(pPanel);
+    }
+    #endregion
+
+
+    #region Event Handler
+    public void OnEventToChangeScene(eSceneType eCurrentScene, eSceneType eNextScene)
+    {
+        var pDestroyPanels = new DicPanels();
+        SHUtils.ForToDic(m_dicPanels, (pKey, pValue) =>
+        {
+            if (eObjectDestoryType.ChangeScene != pValue.m_eDestroyType)
+                return;
+
+            pDestroyPanels.Add(pKey, pValue);
+        });
+        
+        DestoryPanel(pDestroyPanels);
+    }
+    #endregion
+
+
+    #region Temp Functions
+    public void ShowNotice_NoMake()
+    {
+        Show("Panel - Notice", new NoticeUI_Param()
+        {
+            m_eButtonType   = eNoticeButton.One,
+            m_eIconType     = eNoticeIcon.Information,
+            m_strTitle      = "알림",
+            m_strMessage    = "업데이트 예정입니다!",
+        });
+    }
+    #endregion
+}
