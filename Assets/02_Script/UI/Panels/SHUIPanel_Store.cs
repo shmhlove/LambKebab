9bee9228b9b70a80c537d69c171f72d6ac37e3bf
shmhlove <shmhlove@naver.com>
2016-12-09 17:44:48 +0900

    [추가] 상점 준비

---
+++ Assets/02_Script/UI/Panels/SHUIPanel_Store.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class SHUIPanel_Store : SHUIBasePanel
+{
+    #region Members : Inspector
+    #endregion
+
+
+    #region Members : Event
+    #endregion
+
+
+    #region System Functions
+    #endregion
+
+
+    #region Virtual Functions
+    public override void OnBeforeShow(params object[] pArgs)
+    {
+        
+    }
+    #endregion
+
+
+    #region Interface Functions
+    #endregion
+
+
+    #region Utility Functions
+    #endregion
+
+
+    #region Event Handler
+    public void OnClickToStartGame()
+    {
+    }
+	public void OnClickToStore()
+	{
+	}
+    #endregion
+}
