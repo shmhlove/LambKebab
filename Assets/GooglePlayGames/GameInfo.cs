eef763b4101297363c41d9437849fe217d3117db
shmhlove <shmhlove@naver.com>
2017-01-01 03:25:39 +0900

    [추가] 구글 서비스 설정

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -37,14 +37,14 @@ namespace GooglePlayGames {
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
         private const string UnescapedRequireGooglePlus = "REQUIRE_GOOGLE_PLUS";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "125348741650"; // Filled in automatically
         public const string IosClientId = ""; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "125348741650-dode7h6sbt64s7nonretkirbh01glfjj.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool RequireGooglePlus()
         {
-            return "" == "true";
+            return "false" == "true";
         }
 
         public static bool ApplicationIdInitialized() {
